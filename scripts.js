99e4873ad92eeffa5fbdd9e8efd5904373b4347c
uchihaitachi5099 <118080966+uchihaitachi5099@users.noreply.github.com>
2024-06-21 01:27:09 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+});
